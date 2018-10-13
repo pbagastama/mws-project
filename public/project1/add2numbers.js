f73c7f02a3c5825584590b92d19a0465f0e41298
tamabagas <pbagastama@gmail.com>
2018-10-13 08:01:27 +0700

    Update Kalkulator

--- public/project1/add2numbers.js
+++ public/project1/add2numbers.js
@@ -1,8 +1,27 @@
-function kalkulator() {
+function tambah() {
    let angka = document.querySelectorAll('input');
    let i1= parseInt(angka[0].value); //angka pertama
    let i2= parseInt(angka[1].value); //angka kedua
-   angka[2].value= parseInt(i1) + parseInt(i2) ;
+   angka[2].value= parseInt(i1) + parseInt(i2);
 }
-let tombol = document.querySelector('button');
-tombol.addEventListener('click', kalkulator);
+
+function kurang() {
+    let angka2 = document.querySelectorAll('input');
+    let i1= parseInt(angka2[3].value); //angka pertama
+    let i2= parseInt(angka2[4].value); //angka kedua
+    angka2[5].value= parseInt(i1) - parseInt(i2) ;
+ }
+
+ function kali() {
+    let angka3 = document.querySelectorAll('input');
+    let i1= parseInt(angka3[6].value); //angka pertama
+    let i2= parseInt(angka3[7].value); //angka kedua
+    angka3[8].value= parseInt(i1) * parseInt(i2) ;
+ }
+
+ function bagi() {
+    let angka = document.querySelectorAll('input');
+    let i1= parseInt(angka[9].value); //angka pertama
+    let i2= parseInt(angka[10].value); //angka kedua
+    angka[11].value= parseInt(i1) / parseInt(i2) ;
+ }
